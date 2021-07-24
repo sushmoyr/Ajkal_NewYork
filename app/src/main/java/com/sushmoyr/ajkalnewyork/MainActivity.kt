1120ebfc47c5376ed8d75a8434e3c435492eea53
sushmoy <sushmoyr@gmail.com>
2021-07-24 20:02:54 +0600

    Setup Navigation graph

--- app/src/main/java/com/sushmoyr/ajkalnewyork/MainActivity.kt
+++ app/src/main/java/com/sushmoyr/ajkalnewyork/MainActivity.kt
@@ -2,11 +2,39 @@ package com.sushmoyr.ajkalnewyork
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.sushmoyr.ajkalnewyork.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navHostFragment: NavHostFragment
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        navHostFragment = (supportFragmentManager.findFragmentById(R.id.main_nav_host) as NavHostFragment)
+        navController = navHostFragment.navController
+        val toolbar = binding.toolbar
+
+        setSupportActionBar(toolbar)
+
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        setUpBottomNavigation()
+    }
+
+    private fun setUpBottomNavigation(){
+        val bottomNavigationView = binding.mainBottomNav
+        bottomNavigationView.setupWithNavController(navController)
+
+        bottomNavigationView.setOnNavigationItemReselectedListener {
+            //disabled reselect
+        }
     }
 }

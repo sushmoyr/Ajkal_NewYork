173a3b8cd27742f6667411863b6367b6f511570f
sushmoy <sushmoyr@gmail.com>
2021-07-24 19:25:07 +0600

    Initial commit

---
+++ app/src/main/java/com/sushmoyr/ajkalnewyork/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.sushmoyr.ajkalnewyork
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.sushmoyr.ajkalnewyork.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

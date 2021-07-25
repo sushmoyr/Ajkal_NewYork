279acc378160503048eebfe91e102fa9f5a125e9
sushmoy <sushmoyr@gmail.com>
2021-07-25 12:22:21 +0600

    chip group

--- app/src/main/java/com/sushmoyr/ajkalnewyork/fragments/HomeFragment.kt
+++ app/src/main/java/com/sushmoyr/ajkalnewyork/fragments/HomeFragment.kt
@@ -1,11 +1,22 @@
 package com.sushmoyr.ajkalnewyork.fragments
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.core.view.children
+import com.google.android.material.chip.Chip
+import com.google.gson.Gson
+import com.google.gson.stream.JsonWriter
+import com.sushmoyr.ajkalnewyork.R
 import com.sushmoyr.ajkalnewyork.databinding.FragmentHomeBinding
+import com.sushmoyr.ajkalnewyork.models.Category
+import com.sushmoyr.ajkalnewyork.models.News
+import kotlin.random.Random
+import kotlin.random.nextInt
 
 class HomeFragment : Fragment() {
 
@@ -18,9 +29,63 @@ class HomeFragment : Fragment() {
 
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
 
+        setUpSelectionChips()
 
+        setUpRecyclerView()
 
         return binding.root
+    }
+
+    private fun setUpRecyclerView() {
+        TODO("Not yet implemented")
+    }
+
+    private fun setUpSelectionChips() {
+        val chipGroup = binding.newsFilterChipGroup
+        val filters = resources.getStringArray(R.array.news_filters)
+        var isChecked = false
+        filters.forEach {
+            val chip = Chip(requireContext())
+            chip.id = View.generateViewId()
+            chip.text = it
+            chipGroup.addView(chip)
+
+            if(!isChecked){
+                chipGroup.check(chip.id)
+                chip.chipStrokeWidth = 0f
+                chip.setChipBackgroundColorResource(R.color.secondaryColor)
+                chip.setTextColor(ContextCompat.getColor(requireContext(), R.color.white))
+                isChecked = true
+            }
+        }
+
+        chipGroup.isSingleSelection = true
+
+
+        val placeHolderChip = Chip(requireContext())
+
+        chipGroup.setOnCheckedChangeListener { group, checkedId ->
+            group.children.forEach { chip->
+                if(chip is Chip){
+                    if(chip.id == checkedId){
+                        chip.chipStrokeWidth = 0f
+                        chip.setChipBackgroundColorResource(R.color.secondaryColor)
+                        chip.setTextColor(ContextCompat.getColor(requireContext(), R.color.white))
+                    }
+                    else{
+                        chip.chipBackgroundColor = placeHolderChip.chipBackgroundColor
+                        chip.chipStrokeWidth = placeHolderChip.chipStrokeWidth
+                        chip.setTextColor(ContextCompat.getColor(requireContext(), R.color.black))
+                    }
+                }
+            }
+            val chip = view?.findViewById<Chip>(checkedId)
+            if (chip != null) {
+                chip.chipStrokeWidth = 0f
+                chip.setChipBackgroundColorResource(R.color.secondaryColor)
+            }
+
+        }
     }
 
     override fun onDestroy() {
